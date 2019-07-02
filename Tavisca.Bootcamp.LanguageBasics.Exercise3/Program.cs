fe79b35812e8f7287b062d2f772ff3e5ad3fc3a3
tavisca-jkanathe <51927665+tavisca-jkanathe@users.noreply.github.com>
2019-07-02 14:00:23 +0530

    Update Program.cs

--- Tavisca.Bootcamp.LanguageBasics.Exercise3/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise3/Program.cs
@@ -39,58 +39,58 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
             Console.WriteLine(result);
         }
 
-         public static int[] find_max(int[] index_array, int[] diet)
+         public static int[] FindMax(int[] indexArray, int[] diet)
         {
-            List<int> new_index_array = new List<int>();// To maintain list of new diet plan
-            int temp = diet[index_array[0]];
-           new_index_array.Add(index_array[0]);
+            List<int> newIndexArray = new List<int>();// To maintain list of new diet plan
+            int temp = diet[indexArray[0]];
+           newIndexArray.Add(indexArray[0]);
             //find list of high diets
-            for(int i=1; i<index_array.Length;i++)
+            for(int i=1; i<indexArray.Length;i++)
             {
-                if(diet[index_array[i]]>temp)
+                if(diet[indexArray[i]]>temp)
                 {
-                    temp=diet[index_array[i]];
-                    //Array.Clear(new_index_array);
-                    new_index_array.Clear();
-                    new_index_array.Add(index_array[i]);
+                    temp=diet[indexArray[i]];
+                    //Array.Clear(newIndexArray);
+                    newIndexArray.Clear();
+                    newIndexArray.Add(indexArray[i]);
                     
                 }
-                else if(diet[index_array[i]]==temp)
+                else if(diet[indexArray[i]]==temp)
                 {
-                   new_index_array.Add(index_array[i]);
+                   newIndexArray.Add(indexArray[i]);
                 }
             }
-            new_index_array.Sort();// sort diet plan list according to index
-            return new_index_array.ToArray();
+            newIndexArray.Sort();// sort diet plan list according to index
+            return newIndexArray.ToArray();
         }
-        public static int[] find_min(int[] index_array, int[] diet)
+        public static int[] FindMin(int[] indexArray, int[] diet)
         {
-            List<int> new_index_array = new List<int>();// To maintain list of new diet plan
-            int temp = diet[index_array[0]];
-           new_index_array.Add(index_array[0]);
+            List<int> newIndexArray = new List<int>();// To maintain list of new diet plan
+            int temp = diet[indexArray[0]];
+           newIndexArray.Add(indexArray[0]);
             //find list of low diets
-            for(int i=1; i<index_array.Length;i++)
+            for(int i=1; i<indexArray.Length;i++)
             {
-                if(diet[index_array[i]]<temp)
+                if(diet[indexArray[i]]<temp)
                 {
-                    temp=diet[index_array[i]];
-                    new_index_array.Clear();
-                    new_index_array.Add(index_array[i]);
+                    temp=diet[indexArray[i]];
+                    newIndexArray.Clear();
+                    newIndexArray.Add(indexArray[i]);
                     
                 }
-                else if(diet[index_array[i]]==temp)
+                else if(diet[indexArray[i]]==temp)
                 {
-                   new_index_array.Add(index_array[i]);
+                   newIndexArray.Add(indexArray[i]);
                 }
             }
-            new_index_array.Sort();// sort diet plan list according to index
-            return new_index_array.ToArray();
+            newIndexArray.Sort();// sort diet plan list according to index
+            return newIndexArray.ToArray();
         }
         
         public static int[] SelectMeals(int[] protein, int[] carbs, int[] fat, string[] dietPlans)
         {
             // Add your code here.
-             int[] calorie =new int[protein.Length];//to store calorie count
+            int[] calorie =new int[protein.Length];//to store calorie count
             int[] result =new int[dietPlans.Length];// to store final output
             int r=0;
             //To calculate calorie count
@@ -101,56 +101,56 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
             //diet plan of each member
             for(int i=0;i<dietPlans.Length;i++)
             {
-                int[] index_array =new int[protein.Length];// To store all possible diet plans index
+                int[] indexArray =new int[protein.Length];// To store all possible diet plans index
                 //choose all diet plans initially
                 for(int j=0;j<protein.Length;j++)
                     {
-                      index_array[j]= j;
+                      indexArray[j]= j;
                     }
                 //Choose best diet plans according to diet code  one by one
-                //Possibly Reducing index_array size after each loop
+                //Possibly Reducing indexArray size after each loop
                 for(int j=0;j<dietPlans[i].Length;j++)
                 {
-                    char diet_code= dietPlans[i][j];
+                    char dietCode= dietPlans[i][j];
                     
-                    if(diet_code== 'P')
+                    if(dietCode== 'P')
                     {
-                        index_array = find_max(index_array, protein);
+                        indexArray = FindMax(indexArray, protein);
                     }
-                    else if(diet_code== 'p')
+                    else if(dietCode== 'p')
                     {
-                        index_array = find_min(index_array, protein);
+                        indexArray = FindMin(indexArray, protein);
                     }
-                    else if(diet_code== 'C')
+                    else if(dietCode== 'C')
                     {
-                        index_array = find_max(index_array, carbs);
+                        indexArray = FindMax(indexArray, carbs);
                     }
-                    else if(diet_code== 'c')
+                    else if(dietCode== 'c')
                     {
-                        index_array = find_min(index_array, carbs);
+                        indexArray = FindMin(indexArray, carbs);
                     }
-                    else if(diet_code== 'F')
+                    else if(dietCode== 'F')
                     {
-                        index_array = find_max(index_array, fat);
+                        indexArray = FindMax(indexArray, fat);
                     }
-                    else if(diet_code== 'f')
+                    else if(dietCode== 'f')
                     {
-                        index_array = find_min(index_array, fat);
+                        indexArray = FindMin(indexArray, fat);
                     }
-                    else if(diet_code== 'T')
+                    else if(dietCode== 'T')
                     {
-                        index_array = find_max(index_array, calorie);
+                        indexArray = FindMax(indexArray, calorie);
                     }
-                    else if(diet_code== 't')
+                    else if(dietCode== 't')
                     {
-                        index_array = find_min(index_array, calorie);
+                        indexArray = FindMin(indexArray, calorie);
                     }
                     //break loop if no further diet code check required
-                    if(index_array.Length==1)
+                    if(indexArray.Length==1)
                         break;
                 }
                 //store best diet plan of each member in result array
-                result[r++]=index_array[0];
+                result[r++]=indexArray[0];
             }
             return result;
         }
